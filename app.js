558d524b2d45b1549a0fcd0aa03ce9d2669f38e0
Areeb <zahooriareeb@gmail.com>
2023-12-11 22:29:38 +0530

    wrote some js

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const startOrContinueButton = document.querySelector("#startOrContinueButton");
 const newGameButton = document.querySelector("#newGameButton");
 let gameStarted = false;
 const homeButton = document.querySelector("#homeButton");
-const resetboardButton = document.querySelector("#resetboardButton");
+const resetBoardButton = document.querySelector("#resetBoardButton");
 const undoButton = document.querySelector("#undoButton");
 const popUpBg = document.querySelector("#popUpBg");
 const popUp = document.querySelector("#popUp");
 const popUpYes = document.querySelector("#yes");
 const popUpNo = document.querySelector("#no");
+let prevBoardConfiguration;
 let activeBoard;
-let preservedBoards;
+let preservedBoards = [];
+const up = document.querySelector("#up");
+const left = document.querySelector("#left");
+const right = document.querySelector("#right");
+const down = document.querySelector("#down");
+let moveInProcess = false;
 
-//* Home Page
+//! Home Page
 leftC.addEventListener("click", () => {
 	size = size == 3 ? 8 : --size;
 	sizeTag.innerHTML = `${size}&nbsp;x&nbsp;${size}`;
@@ -28,22 +34,24 @@ rightC.addEventListener("click", () => {
 });
 startOrContinueButton.addEventListener("click", () => {
 	gameStarted = true;
-	homePage.classList.add("hidden");
-	playPage.classList.remove("hidden");
 	if (startOrContinueButton.innerText == "Start Game") {
-		activeBoard = new Array(size).fill().map(() => new Array(size).fill(0));
-		generateRandomCell();
-		generateRandomCell();
+		startGame();
 	} else {
 		//? inner text is "Continue Game"
 		activeBoard = preservedBoards.find((matrix) => matrix.length === size);
 		preservedBoards = preservedBoards.filter(
 			(matrix) => matrix !== activeBoard
 		);
+		homeAndPlayPageSwap();
+		makeBoard(false);
 	}
 });
+newGameButton.addEventListener("click", (e) => {
+	preservedBoards = preservedBoards.filter((arr) => arr.length !== size);
+	startGame();
+});
 
-//* Pop Up
+//! Pop Up
 const isPopUpOpen = () => {
 	return !popUpBg.classList.contains("hidden");
 };
@@ -59,7 +67,7 @@ popUpNo.addEventListener("click", () => {
 	popUpToggle();
 });
 
-//* Play Page
+//! Play Page
 homeButton.addEventListener("click", (e) => {
 	gameStarted = false;
 	playPage.classList.add("hidden");
@@ -67,13 +75,45 @@ homeButton.addEventListener("click", (e) => {
 	preservedBoards.push(activeBoard.map((innerArray) => [...innerArray]));
 	preservedBoardExists();
 });
-resetboardButton.addEventListener("click", () => {
+resetBoardButton.addEventListener("click", () => {
 	popUpToggle();
 });
+undoButton.addEventListener("click", () => {
+	activeBoard = prevBoardConfiguration.map((row) => [...row]);
+	makeBoard(false);
+});
+up.addEventListener("click", arrowUpMove);
+left.addEventListener("click", arrowLeftMove);
+right.addEventListener("click", arrowRightMove);
+down.addEventListener("click", arrowDownMove);
+document.addEventListener("keydown", (e) => {
+	if (!gameStarted || isPopUpOpen() || moveInProcess) return;
+	if (e.key === "ArrowUp") arrowUpMove();
+	if (e.key === "ArrowLeft") arrowLeftMove();
+	if (e.key === "ArrowRight") arrowRightMove();
+	if (e.key === "ArrowDown") arrowDownMove();
+});
 
-//* Functions
-const generateRandomCell = () => {};
-const preservedBoardExists = () => {
+//! Functions
+function appearRandomCell(i, j, val) {}
+function generateRandomCell() {
+	const val = Math.floor(Math.random() * 2 + 1) * 2;
+	let i, j;
+	while (activeBoard[i][j] != 0) {
+		i = Math.floor(Math.random() * size);
+		j = Math.floor(Math.random() * size);
+	}
+	activeBoard[i][j] = val;
+	appearRandomCell(i, j, val);
+}
+function homeAndPlayPageSwap() {
+	homePage.classList.toggle("hidden");
+	playPage.classList.toggle("hidden");
+}
+function makeBoard(generateRandomCells) {
+	
+}
+function preservedBoardExists() {
 	if (preservedBoards.find((matrix) => matrix.length === size)) {
 		startOrContinueButton.innerText = "Continue Game";
 		newGameButton.classList.remove("hidden");
@@ -81,5 +121,22 @@ const preservedBoardExists = () => {
 		startOrContinueButton.innerText = "Start Game";
 		newGameButton.classList.add("hidden");
 	}
-};
-const resetBoard = () => {};
+}
+function startGame() {
+	activeBoard = new Array(size).fill().map(() => new Array(size).fill(0));
+	homeAndPlayPageSwap();
+	makeBoard(true);
+}
+function resetBoard() {}
+function arrowUpMove() {
+	console.log(1);
+}
+function arrowLeftMove() {
+	console.log(2);
+}
+function arrowRightMove() {
+	console.log(3);
+}
+function arrowDownMove() {
+	console.log(4);
+}
